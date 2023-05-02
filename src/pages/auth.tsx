1c25c641a92f016f8fbc5010e328d6f23c6e2cb2
marovichn <nikolamarovic.11@gmail.com>
2023-05-02 20:22:48 +0200

    starting-with-netflic-cloning

---
+++ src/pages/auth.tsx
@@ -0,0 +1,16 @@
+
+
+const Auth =()=>{
+return <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
+    <div className="bg-black w-full h-full lg:bg-opacity-60">
+        <nav className="px-12 py-5">
+            <img className="h-12" src="/images/logo.png" alt="" />
+        </nav>
+        <div className="flex justify-center">
+            <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:2-2/5 lg:max-w-md rounded-md w-full"></div>
+        </div>
+    </div>
+</div>
+
+};
+export default Auth;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-2xl text-green-500">Hello World</h1>
     </>
   )
 }
