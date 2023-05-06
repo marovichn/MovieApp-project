3547cec9646bc941f709f3171cd0b00e27004072
marovichn <nikolamarovic.11@gmail.com>
2023-05-06 22:16:02 +0200

    protecting-routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
+import { NextPageContext } from "next"
+import { getSession, signOut } from "next-auth/react"
+import useCurrentUser from "../../hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session= await getSession(context);
+
+  if(!session){
+  return {
+    redirect:{
+      destination: "/auth",
+      permanent: false,
+    }
+  }  
+  }
+
+  return {
+    props:{},
+  }
+}
+
 export default function Home() {
+const { data : user } = useCurrentUser();
+
   return (
     <>
-      <h1 className="text-2xl text-green-500">Hello World</h1>
+      <h1 className="text-6xl text-green-500">Hello {user?.name.split(" ")[0]} !</h1>
+      <button className="h-10 w-full bg-white" onClick={()=>signOut()}>Logout!</button>
     </>
   )
 }
