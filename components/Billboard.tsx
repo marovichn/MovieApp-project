2e8baf1beaf5cee11eb64a966fab31e4238a6b4d
marovichn <nikolamarovic.11@gmail.com>
2023-05-08 23:22:28 +0200

    fetching-data-billboard-display-UI

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import useBillboard from "../hooks/useBillboard";
 
 const Billboard: React.FC=()=>{
-    return <div>
+    const {data} = useBillboard();
 
-    </div>
+
+    return (
+      <div className='relative h-[56.25vw]'>
+        <video
+          className='
+          w-full 
+          h-[56.25vw]
+          object-cover
+          brightness-[60%]'
+          autoPlay
+          muted
+          loop
+          poster={data?.thumbnailUrl}
+          src={data?.videoUrl}
+        ></video>
+        <div className="absolute">
+
+        </div>
+      </div>
+    );
 };
 export default Billboard;
